6b4452189254761f814dac1bae601dc8d29e59f7
Adam Clayman <adam@clayman.org>
2017-09-28 15:47:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-const app = "I don't do much."
+const app = "I don't do much.";
 
-var kittens = ['Milo', 'Otis', 'Garfield'];
-
-function destructivelyPrependKitten(kittens, kitten) {
-  kittens.unshift(kitten);
-  return kittens;
-}
-function destructivelyAppendKitten(kittens, kitten) {
-  kittens.push(kitten);
-  return kittens;
-}
-function destructivelyRemoveFirstKitten(kittens) {
-  kittens.shift();
-  return kittens;
-}
-function destructivelyRemoveLastKitten(kittens) {
-  kittens.pop();
-  return kittens;
-}
-function prependKitten(kittens, kitten) {
-  var kittens2 = [kitten, ...kittens];
-  return kittens2;
-}
-function appendKitten(kittens, kitten) {
-  var kittens2 = [...kittens, kitten];
-  return kittens2;
-}
-function removeFirstKitten(kittens) {
-  var kittens2 = kittens.slice(1);
-  return kittens2;
-}
-function removeLastKitten(kittens) {
-  var kittens2 = kittens.slice(0, kittens.length - 1);
-  return kittens2;
-}
+let kittens = ['Milo', 'Otis', 'Garfield'];
