1479085e195c81db514d784d05149224ca4f1f5a
Adam Clayman <adam@clayman.org>
2017-09-28 15:50:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const app = "I don't do much.";
 
-let kittens = ['Milo', 'Otis', 'Garfield'];
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyPrependKitten(kitten) {
+  return kittens.unshift(kitten);
+}
+function destructivelyAppendKitten(kitten) {
+  return kittens.shift();
+}
+
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
